79ad4ca9232e96c4239c197152294b6c5a5eaf0a
C4r50nC <xichen059@gmail.com>
2024-05-25 16:24:05 +1000

    feat: implement TodoItem deletion

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -15,6 +15,11 @@ const initialTodos = [
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
+
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => (
