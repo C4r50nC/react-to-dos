ad62a93d7ce3461c81952343d3518c3607efa2d2
C4r50nC <xichen059@gmail.com>
2024-05-25 18:32:12 +1000

    feat: add local storage for to-dos

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "@mui/material/List";
 
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
 
-const initialTodos = [
-  { id: 1, text: "walk the dog", completed: false },
-  { id: 2, text: "walk the cat", completed: false },
-  { id: 3, text: "walk the fish", completed: true },
-  { id: 4, text: "walk the chicken", completed: false },
-  { id: 5, text: "walk the bird", completed: true },
-  { id: 6, text: "walk the cow", completed: false },
-  { id: 7, text: "walk the pig", completed: false },
-];
+const getInitialData = () => {
+  const initialData = JSON.parse(localStorage.getItem("todos"));
+  if (!initialData) return [];
+  return initialData;
+};
 
 export default function TodoList() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState(getInitialData);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const removeTodo = (id) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
